21d69e1ad525fbd175211a30b8b888057f11ab96
Matheus Resende <matheushrm614@gmail.com>
2024-01-02 09:55:05 -0300

    Adicionar arquivos de projeto.

---
+++ CRUD_mysql/Form1.Designer.cs
@@ -0,0 +1,305 @@
+﻿namespace CRUD_mysql
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnAdicionar = new System.Windows.Forms.Button();
+            this.inptName = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.inptEmail = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.inptEstado = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.inptBairro = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.inptCidade = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.inptTelefone = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.inptID = new System.Windows.Forms.TextBox();
+            this.btnEditar = new System.Windows.Forms.Button();
+            this.btnRemover = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.Column1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.btnConfirmar = new System.Windows.Forms.Button();
+            this.btnCancelar = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnAdicionar
+            // 
+            this.btnAdicionar.Location = new System.Drawing.Point(17, 169);
+            this.btnAdicionar.Name = "btnAdicionar";
+            this.btnAdicionar.Size = new System.Drawing.Size(75, 23);
+            this.btnAdicionar.TabIndex = 0;
+            this.btnAdicionar.Text = "Adicionar";
+            this.btnAdicionar.UseVisualStyleBackColor = true;
+            this.btnAdicionar.Click += new System.EventHandler(this.btnAdicionar_Click);
+            // 
+            // inptName
+            // 
+            this.inptName.Location = new System.Drawing.Point(80, 17);
+            this.inptName.Name = "inptName";
+            this.inptName.Size = new System.Drawing.Size(510, 20);
+            this.inptName.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(14, 19);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(50, 18);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Nome";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(14, 45);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(48, 18);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Email";
+            // 
+            // inptEmail
+            // 
+            this.inptEmail.Location = new System.Drawing.Point(80, 43);
+            this.inptEmail.Name = "inptEmail";
+            this.inptEmail.Size = new System.Drawing.Size(510, 20);
+            this.inptEmail.TabIndex = 3;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(434, 123);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(58, 18);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "Estado";
+            // 
+            // inptEstado
+            // 
+            this.inptEstado.Location = new System.Drawing.Point(498, 121);
+            this.inptEstado.Name = "inptEstado";
+            this.inptEstado.Size = new System.Drawing.Size(92, 20);
+            this.inptEstado.TabIndex = 7;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(14, 123);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(51, 18);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "Bairro";
+            // 
+            // inptBairro
+            // 
+            this.inptBairro.Location = new System.Drawing.Point(80, 121);
+            this.inptBairro.Name = "inptBairro";
+            this.inptBairro.Size = new System.Drawing.Size(335, 20);
+            this.inptBairro.TabIndex = 5;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(14, 97);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(60, 18);
+            this.label5.TabIndex = 12;
+            this.label5.Text = "Cidade";
+            // 
+            // inptCidade
+            // 
+            this.inptCidade.Location = new System.Drawing.Point(80, 95);
+            this.inptCidade.Name = "inptCidade";
+            this.inptCidade.Size = new System.Drawing.Size(510, 20);
+            this.inptCidade.TabIndex = 11;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(14, 71);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(66, 18);
+            this.label6.TabIndex = 10;
+            this.label6.Text = "Telefone";
+            // 
+            // inptTelefone
+            // 
+            this.inptTelefone.Location = new System.Drawing.Point(80, 69);
+            this.inptTelefone.Name = "inptTelefone";
+            this.inptTelefone.Size = new System.Drawing.Size(377, 20);
+            this.inptTelefone.TabIndex = 9;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.Location = new System.Drawing.Point(469, 71);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(23, 18);
+            this.label7.TabIndex = 14;
+            this.label7.Text = "ID";
+            // 
+            // inptID
+            // 
+            this.inptID.BackColor = System.Drawing.SystemColors.Window;
+            this.inptID.Location = new System.Drawing.Point(498, 69);
+            this.inptID.Name = "inptID";
+            this.inptID.Size = new System.Drawing.Size(92, 20);
+            this.inptID.TabIndex = 13;
+            // 
+            // btnEditar
+            // 
+            this.btnEditar.Location = new System.Drawing.Point(231, 169);
+            this.btnEditar.Name = "btnEditar";
+            this.btnEditar.Size = new System.Drawing.Size(75, 23);
+            this.btnEditar.TabIndex = 16;
+            this.btnEditar.Text = "Editar";
+            this.btnEditar.UseVisualStyleBackColor = true;
+            // 
+            // btnRemover
+            // 
+            this.btnRemover.Location = new System.Drawing.Point(124, 169);
+            this.btnRemover.Name = "btnRemover";
+            this.btnRemover.Size = new System.Drawing.Size(75, 23);
+            this.btnRemover.TabIndex = 17;
+            this.btnRemover.Text = "Remover";
+            this.btnRemover.UseVisualStyleBackColor = true;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Column1,
+            this.Column2});
+            this.dataGridView1.Location = new System.Drawing.Point(12, 213);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(596, 225);
+            this.dataGridView1.TabIndex = 18;
+            // 
+            // Column1
+            // 
+            this.Column1.HeaderText = "Column1";
+            this.Column1.Name = "Column1";
+            // 
+            // Column2
+            // 
+            this.Column2.HeaderText = "Column2";
+            this.Column2.Name = "Column2";
+            // 
+            // btnConfirmar
+            // 
+            this.btnConfirmar.Location = new System.Drawing.Point(515, 169);
+            this.btnConfirmar.Name = "btnConfirmar";
+            this.btnConfirmar.Size = new System.Drawing.Size(75, 23);
+            this.btnConfirmar.TabIndex = 19;
+            this.btnConfirmar.Text = "Confirmar";
+            this.btnConfirmar.UseVisualStyleBackColor = true;
+            this.btnConfirmar.Visible = false;
+            // 
+            // btnCancelar
+            // 
+            this.btnCancelar.Location = new System.Drawing.Point(417, 169);
+            this.btnCancelar.Name = "btnCancelar";
+            this.btnCancelar.Size = new System.Drawing.Size(75, 23);
+            this.btnCancelar.TabIndex = 20;
+            this.btnCancelar.Text = "Cancelar";
+            this.btnCancelar.UseVisualStyleBackColor = true;
+            this.btnCancelar.Visible = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(620, 450);
+            this.Controls.Add(this.btnCancelar);
+            this.Controls.Add(this.btnConfirmar);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.btnRemover);
+            this.Controls.Add(this.btnEditar);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.inptID);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.inptCidade);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.inptTelefone);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.inptEstado);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.inptBairro);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.inptEmail);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.inptName);
+            this.Controls.Add(this.btnAdicionar);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnAdicionar;
+        private System.Windows.Forms.TextBox inptName;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox inptEmail;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox inptEstado;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox inptBairro;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox inptCidade;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox inptTelefone;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox inptID;
+        private System.Windows.Forms.Button btnEditar;
+        private System.Windows.Forms.Button btnRemover;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column2;
+        private System.Windows.Forms.Button btnConfirmar;
+        private System.Windows.Forms.Button btnCancelar;
+    }
+}
+
---
+++ CRUD_mysql/Form1.cs
@@ -0,0 +1,50 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CRUD_mysql
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnAdicionar_Click(object sender, EventArgs e)
+        {
+            string nome = inptName.Text,
+                email = inptEmail.Text,
+                telefone = inptTelefone.Text,
+                cidade = inptCidade.Text,
+                bairro = inptBairro.Text,
+                estado = inptEstado.Text;
+
+            if (nome.Equals("") &&
+                email.Equals("") &&
+                telefone.Equals("") &&
+                bairro.Equals("") &&
+                estado.Equals("") &&
+                cidade.Equals(""))
+            {
+                Console.WriteLine("Preencha todos os campos");
+                return;
+            }
+
+            Console.WriteLine("valeu");
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            var cadastro = new Cadastro();
+            cadastro.TestaConexao();
+        }
+    }
+}
